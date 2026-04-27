36056547dcdde92699189e32b133734ea1feddcd
Athaaya Fauzan <athaayafauzan@gmail.com>
2026-04-27 09:29:05 +0700

    fix

--- soal1.c
+++ soal1.c
@@ -8,23 +8,27 @@
 */
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-int perbandingan(const void *a, const void *b){
+int perbandingan(const void *a, const void *b) {
     return (*(int*)a - *(int*)b);
 }
 
 int main(void){
-    int *data = NULL;
+    int *data= NULL;
     int count = 0;
     int kapasitas = 0;
     int input;
 
-    while(scanf("%d", &input) == 1 && input != -1){
-        if(count == kapasitas){
+    while (scanf("%d", &input) == 1 && input != -1){
+        if (count == kapasitas){
             kapasitas = (kapasitas == 0) ? 1 : kapasitas * 2;
-            data = (int*)realloc(data, kapasitas * sizeof(int));
+            int *temp = (int*)realloc(data, kapasitas * sizeof(int));
+            if (temp == NULL){
+                free(data);
+                return 1;
+            }
+            data = temp;
         }
         data[count] = input;
         count++;
@@ -38,19 +42,20 @@ int main(void){
     qsort(data, count, sizeof(int), perbandingan);
 
     printf("COUNT %d ", count);
+
     printf("SORTED");
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++){
         printf(" %d", data[i]);
     }
 
     double median;
     if (count % 2 == 0){
         median = (data[count/2 - 1] + data[count/2]) / 2.0;
-    } else {
+    }else{
         median = (double)data[count/2];
     }
-    
-    printf(" MEDIAN %g\n", median);
+
+    printf(" MEDIAN %.2f\n", median);
 
     free(data);
     return 0;
